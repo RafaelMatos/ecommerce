2dbd70c93c002bee82eb800ccab6ceacf761d0c6
RafaelMatos <rafael-matos@outlook.com>
2019-08-06 00:34:37 -0300

    Generate the Order ID

--- ecommerce/orders/models.py
+++ ecommerce/orders/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
+from django.db.models.signals import pre_save
 from carts.models import Cart
-
+from ecommerce.utils import unique_order_id_generator
 
 ORDER_STATUS_CHOICES = (
     ('created','Created'),
@@ -12,7 +12,7 @@ ORDER_STATUS_CHOICES = (
 # Create your models here.
 class Order(models.Model):
 
-    order_id       = models.CharField(max_length=120) # AB31DE3
+    order_id       = models.CharField(max_length=120,blank = True) # AB31DE3
     # billing_profile = ?
     # shipping_address = ?
     #  billing_address = ?
@@ -23,6 +23,14 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_id
+    
+def pre_save_create_order_id(sender, instance, *args,**kwargs):
+    if not instance.order_id:
+        instance.order_id = unique_order_id_generator(instance)
+        # instance.save()
+        
+pre_save.connect(pre_save_create_order_id,sender=Order)
+
 
 # generate the order id
 # generate the order total

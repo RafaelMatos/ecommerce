8e9626a390b62e69f55fdf9f080a40ea26833d7f
RafaelMatos <rafael-matos@outlook.com>
2019-08-07 17:45:50 -0300

    Calculate the Order Total

--- ecommerce/ecommerce/utils.py
+++ ecommerce/ecommerce/utils.py
@@ -10,6 +10,9 @@ def random_string_generator(size=10, chars=string.ascii_lowercase + string.digit
 
 def unique_order_id_generator(instance):
 
+    """
+    This is for a Django project with an order_id field.
+    """
     order_new_id = random_string_generator()
     Klass = instance.__class__
     qs_exists = Klass.objects.filter(order_id = order_new_id).exists()
@@ -19,9 +22,7 @@ def unique_order_id_generator(instance):
 
 
 def unique_slug_generator(instance, new_slug=None):
-    """
-    This is for a Django project with an order_id field.
-    """
+    
     if new_slug is not None:
         slug = new_slug
     else:
